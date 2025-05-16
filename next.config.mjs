b5c1ca539ed3d2c95551c21f2a438dceb78da058
Kingtilon1 <bobbtilon@gmail.com>
2025-05-15 21:19:08 -0400

    adjusted fro mjs extension

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = {
+const nextConfig = {
     eslint: {
       // Only run ESLint on specific directories during production builds (optional)
       dirs: ['pages', 'components', 'lib', 'src'],
-      // Ignore ESLint errors during production builds (this is a bit extreme but will work)
+      // Ignore ESLint errors during production builds
       ignoreDuringBuilds: true,
-    },
-  }
-export default nextConfig;
+    }
+  };
+  
+  export default nextConfig;
